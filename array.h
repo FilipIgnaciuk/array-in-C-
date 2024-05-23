4d1e76c4b5a7c5c2dbcc168967fc6feee4116a56
Filip Ignaciuk <250965@edu.p.lodz.pl>
2024-05-23 22:24:48 +0200

    array in C++

---
+++ array.h
@@ -0,0 +1,23 @@
+#ifndef ARRAY_C___ARRAY_H
+#define ARRAY_C___ARRAY_H
+#define SIZE 10
+
+using namespace std;
+
+void displayArray(int array[]);
+
+int findMaximumValue(int array[]);
+
+int saveArrayToFile(int array[]);
+
+int findMinimumValue(int array[]);
+
+int enterDataIntoArray(int array[]);
+
+float calculateAverege(int array[]);
+
+void displayFromFile(int array[]);
+
+
+
+#endif //ARRAY_C___ARRAY_H
